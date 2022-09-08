d3ff908ab2163bc798258c28dd44d73d64e40376
yash suvta <yashsuvta@gmail.com>
2022-09-08 15:03:18 +0530

    Configured dpcker copose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x from y and return result."""
     return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -4,11 +4,12 @@ Sample test
 from django.test import SimpleTestCase
 from app import calc
 
+
 class ClassTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
